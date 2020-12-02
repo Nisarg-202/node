9969fcbd6111107bacf582841282faecfb2118a1
nisarg prajapati <nisargprajapati202@gmail.com>
2020-12-02 21:12:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Server is running on port 3000.');
+});
